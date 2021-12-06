f5530890c44fdf7bdccc881c46fe68d100689f15
Akashmungal <mungalakash92@gmail.com>
2021-12-06 14:23:28 +0530

    adding uc-6.sh into UC6

---
+++ uc-6.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+
+
+isPartTime=1
+isFullTime=2
+maxRateInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+
+
+while [[ $totalEmpHr -lt $maxRateInMonth && $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                                $isFullTime)
+                                        empHrs=8
+                                ;;
+                                $isPartTime)
+                                        empHrs=4
+                                ;;
+                                *)
+                                        empHrs=0
+        esac
+
+
+        totalEmpHr=$(($totalEmpHr+$empHrs))
+done
+
+
+totalSalary=$(($totalEmpHr*$empRatePerHr))
